cc23a5ebff323ebfdd7955900f2f6aabc70e01b5
AlanGustav0 <alagustavo946@gmail.com>
2023-02-14 08:43:50 -0300

    feat: Implementation database connextion and UserController

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -9,5 +9,6 @@ namespace api__auth.Models
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
+        public string ProfileStyle { get; set; }
     }
 }
