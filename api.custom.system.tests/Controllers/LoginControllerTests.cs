cc3c5315135e9ceef87b6933841332cc6c52c92a
AlanGustav0 <alagustavo946@gmail.com>
2023-05-04 07:34:24 -0300

    feature: Implements test module configuration

---
+++ api.custom.system.tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,15 @@
+using api__custom_system.Controllers;
+
+namespace api.custom.system.tests
+{
+    public class LoginControllerTests
+    {
+        private readonly LoginController _controller;
+
+        [Fact]
+        public void ShouldReturnTokenResponse()
+        {
+            Assert.True(true);
+        }
+    }
+}
